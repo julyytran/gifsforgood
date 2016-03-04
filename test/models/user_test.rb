ae2c42ab8e379de35610a6dd89bfb8236111bb9c
hhoopes <heidih@gmail.com>
2016-03-03 23:18:14 -0700

    Add shoulda matcher tests on remaining models

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should have_many(:orders)
+  should validate_presence_of(:password)
+  should validate_presence_of(:username)
 end
