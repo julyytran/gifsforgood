ae2df9ae3b951f6e99984a17ee6c99dd7150ba00
hhoopes <heidih@gmail.com>
2016-03-04 23:38:54 -0700

    Finish functionality for creating an order from a cart Finish happy path test for creating an order and seeing it in index, but not sad path yet for not being able to see other users' orders. Added OrderProcessor class and refined the order create controller. Closes #44

--- db/schema.rb
+++ db/schema.rb
@@ -50,10 +50,8 @@ ActiveRecord::Schema.define(version: 20160305035049) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "status"
-    t.integer  "total_price", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
