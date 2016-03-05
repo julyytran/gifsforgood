d4216bbbe6f1c178f008512d487cb82b1fb202ef
Brian Rippeto <rippeto@gmail.com>
2016-03-05 12:54:37 -0700

    fix price formatting and reduce method in order proccessor

--- app/models/order_processor.rb
+++ app/models/order_processor.rb
@@ -11,11 +11,9 @@ class OrderProcessor
 
   def process_order
     order = user.orders.create(total_price: cart.total_price, status: "Pending")
-    binding.pry
     cart_gifs.each do | cart_gif |
       order.order_gifs.create(gif_id: cart_gif.id, quantity: cart_gif.quantity, subtotal: cart_gif.subtotal)
     end
-    binding.pry
     order
   end
 
