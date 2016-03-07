f5e3e988507a1ec661fcfedc461004f0814da264
Brian Rippeto <rippeto@gmail.com>
2016-03-06 20:11:29 -0700

    deleted comments-AIRPLANE

--- test/integration/admin_can_create_item_test.rb
+++ test/integration/admin_can_create_item_test.rb
@@ -27,24 +27,3 @@ end
 # - The title must be unique for all items in the system.
 # - The price must be a valid decimal numeric value and greater than zero.
 # - The photo is optional. If not present, a stand-in photo is used.
-#     create_and_return_admin
-#
-#     visit "/gifs"
-#
-#     click_on "New Item"
-#
-#     fill_in "title", with: "all of teh lulz"
-#     fill_in "description", with: "this is all the lulz you could imagine!!"
-#     fill_in "price", with: "100"
-#     fill_in "tags", with: "lulzy" #HOW TO FILL IN MULTIPLE TAGS???
-#     fill_in "image", with: "http://media2.giphy.com/media/jixtchplI4vYY/giphy.gif"
-#
-#     click_on "Create Item"
-#
-#     assert page.has_content? "all of teh lulz"
-#     assert page.has_content? "this is all the lulz you could imagine!!"
-#     assert page.has_content? "$1.00"
-#     assert page.has_content? "Tags: lulzy"
-#     assert page.has_css?("img[src='http://media2.giphy.com/media/jixtchplI4vYY/giphy.gif']")
-#   end
-# end
