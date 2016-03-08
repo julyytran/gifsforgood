bb484a492295ee1252b7f585be3bcbcb677b7204
July Tran <julyytran@gmail.com>
2016-03-07 20:58:00 -0700

    paperclip and aws working

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -14,15 +14,15 @@ class Gif < ActiveRecord::Base
       favicon: '16x16>',
       square: '200x200#',
       medium: '300x300>'
-    },
-    
-    :storage => :s3,
-      :s3_region => ENV['AWS_REGION'],
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
+
+    # :storage => :s3,
+    #   :s3_region => ENV['AWS_REGION'],
+    # :s3_credentials => {
+    #   :bucket => ENV['S3_BUCKET_NAME'],
+    #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    # }
 
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
